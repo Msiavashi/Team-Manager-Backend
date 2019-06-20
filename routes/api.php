c4bb4a53b8275c45faad163949ad7e6312231851
msiavashi <mohammad.siav@hotmail.com>
2019-06-20 17:12:53 +0430

    auth protection added

--- routes/api.php
+++ routes/api.php
@@ -21,22 +21,18 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::post('login', 'API\UserController@login');
 Route::post('register', 'API\UserController@register');
 
-
-
-// teams
-Route::delete('team/{id}', 'API\TeamController@destroy');
-Route::post('team', 'API\TeamController@create');
-Route::get('team/{id}', 'API\TeamController@show');
-Route::patch('team/{id}', 'API\TeamController@update');
-Route::get('teams', 'API\TeamController@index');
-
-// members
-Route::delete('member/{id}', 'API\MemberController@destroy');
-Route::post('team/{tid}/member', 'API\MemberController@create');
-Route::get('team/{tid}/members', 'API\MemberController@index');
-Route::get('member/{id}', 'API\MemberController@show');
-Route::patch('member/{id}', 'API\MemberController@update');
-
 Route::group(['middleware' => 'auth:api'], function(){
+    // teams
+    Route::delete('team/{id}', 'API\TeamController@destroy');
+    Route::post('team', 'API\TeamController@create');
+    Route::get('team/{id}', 'API\TeamController@show');
+    Route::patch('team/{id}', 'API\TeamController@update');
+    Route::get('teams', 'API\TeamController@index');
 
+    // members
+    Route::delete('member/{id}', 'API\MemberController@destroy');
+    Route::post('team/{tid}/member', 'API\MemberController@create');
+    Route::get('team/{tid}/members', 'API\MemberController@index');
+    Route::get('member/{id}', 'API\MemberController@show');
+    Route::patch('member/{id}', 'API\MemberController@update');
 });
